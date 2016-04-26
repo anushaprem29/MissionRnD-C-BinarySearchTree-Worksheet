b6bfc3d7d921fa7b945e93cc0a9042e19847d105
anushaprem29 <anushaprem29@missionrnd.com>
2016-04-26 20:00:41 +0530

    Submit Commit

--- src/BSTClosestLeafDistance.cpp
+++ src/BSTClosestLeafDistance.cpp
@@ -45,13 +45,13 @@ int min(int a, int b){
 }
 
 int near(struct node *root){
-	if (root == NULL) return 0;
+	if (root == NULL) return 999;
 	if (root->left == NULL && root->right == NULL)	return 0;
 	return 1 + min(near(root->left), near(root->right));
 }
 
 int getClosest(struct node *root, struct node *temp, struct node *parentNodes[], int pos){
-	if (root == NULL)	return 0;
+	if (root == NULL)	return 999;
 	if (root->data == temp->data){
 		int res = near(root), iter;
 		for (iter = pos - 1; iter >= 0; iter--)
@@ -62,8 +62,7 @@ int getClosest(struct node *root, struct node *temp, struct node *parentNodes[],
 	return min(getClosest(root->left, temp, parentNodes, pos + 1), getClosest(root->right, temp, parentNodes, pos + 1));
 }
 
-int get_closest_leaf_distance(struct node *root, struct node *temp)
-{
+int get_closest_leaf_distance(struct node *root, struct node *temp){
 	if (root == NULL || temp == NULL) return -1;
 	if (temp->left == NULL && temp->right == NULL) return 0;
 	struct node *ancestors[100];
