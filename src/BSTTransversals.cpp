7b3fd0a6be0aed40215d268e71e2f8c2f3b6defa
anushaprem29 <anushaprem29@missionrnd.com>
2016-04-26 19:47:32 +0530

    Submit Commit

--- src/BSTTransversals.cpp
+++ src/BSTTransversals.cpp
@@ -9,7 +9,7 @@ Note : Memory for the Array is already allocated ,Dont allocate it again using m
 ->Take help of helper functions which are actually solving the problem .
 ->Dont Process Invalid Inputs
 
-Bonus Task : 
+Bonus Task :
 ->Go to the BSTTransversals Spec File ,We have left a custom test case for you ,Try to fill
 it and understand how testing works .
 */
@@ -21,15 +21,56 @@ struct node{
 	int data;
 	struct node *right;
 };
-
+void inorderT(struct node *r, int *arr, int *pos)
+{
+	if (r != NULL)
+	{
+		inorderT(r->left, arr, pos);
+		arr[(*pos)++] = r->data;
+		inorderT(r->right, arr, pos);
+	}
+	else return;
+}
 
 void inorder(struct node *root, int *arr){
-	
+	if (arr == NULL || root == NULL)	return;
+	int iter = 0;
+	inorderT(root, arr, &iter);
 }
+void preorderT(struct node *r, int *arr, int *pos)
+{
+	if (r != NULL)
+	{
+		arr[*pos] = r->data;
+		(*pos)++;
+		preorderT(r->left, arr, pos);
+		preorderT(r->right, arr, pos);
+	}
+	else return;
+}
+
 void preorder(struct node *root, int *arr){
-	
+
+	if (arr == NULL || root == NULL)	return;
+	int iter = 0;
+	preorderT(root, arr, &iter);
 }
+void postorderT(struct node *r, int *arr, int *pos)
+{
+	if (r != NULL)
+	{
+		postorderT(r->left, arr, pos);
+		postorderT(r->right, arr, pos);
+		arr[*pos] = r->data;
+		(*pos)++;
+	}
+
+	else return;
+}
+
 void postorder(struct node *root, int *arr){
-	
+	if (arr == NULL || root == NULL)	return;
+	int iter = 0;
+	postorderT(root, arr, &iter);
 }
 
